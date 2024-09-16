e3cc404aad7d26b9d26b241e4e696f36e7b66c12
Vu Minh Tuan <tuanvm@dagoras.io>
2024-09-16 11:12:44 +0700

    keyed process function timer to remove expired records

--- functions/functions.py
+++ functions/functions.py
@@ -5,6 +5,7 @@ from pyflink.common import Row
 from typing import Any
 from utils.rule import CardType, AggregationType
 from utils.accumulator_creator import AccumulatorCreator
+import time
 
 class FraudProcessFunction(KeyedBroadcastProcessFunction):
     def __init__(self):
@@ -33,24 +34,44 @@ class FraudProcessFunction(KeyedBroadcastProcessFunction):
     def process_element(self, value: Any, ctx: KeyedBroadcastProcessFunction.ReadOnlyContext):
         card_type = value["card_type"]
         current_timestamp = value["created_at"]
-        self._add_transaction_to_state(event_time=current_timestamp, row=value)
 
         rule = ctx.get_broadcast_state(self._rule_state_desc).get(card_type)
+        if rule == None:
+            return
+        
+        self._add_transaction_to_state(event_time=current_timestamp, row=value)
+        
         window_duration = rule["duration"]
         window_type = rule["type"]
+        rule_amount = rule["amount"]
         
         accumulator = AccumulatorCreator.get_accumulator(type=window_type)
         for event_time in self._window_state.keys():
             if self._transaction_time_is_valid(transaction_time=event_time, start_time=(current_timestamp-window_duration), current_time=current_timestamp):
                 transaction_list = self._window_state.get(event_time)
-                [accumulator.add(t["amount"]) for t in transaction_list]
+                [accumulator.add(t["amount"]) for t in transaction_list if t["card_type"] == card_type]
 
         aggregated_result = accumulator.get_local_value()
+        rule_violated = accumulator.check_rule(rule_amount)
+        if rule_violated:
+            yield Row(value["user_id"], card_type, window_type, f"Rule violated! {aggregated_result} is greater than limit {rule_amount}", int(time.time()))
         
+        ctx.timer_service().register_event_time_timer(current_timestamp * 1000)
 
     # todo
     def on_timer(self, timestamp: int, ctx: KeyedBroadcastProcessFunction.OnTimerContext):
-        pass
+        window = self._window_state
+
+        card_type = ctx.get_current_key()[1]
+        rule = ctx.get_broadcast_state(self._rule_state_desc).get(card_type)
+        window_duration = rule["duration"]
+        limit_timestamp = timestamp - window_duration
+
+        for event_time in window.keys():
+            if event_time < limit_timestamp:
+                self._window_state.remove(event_time)
+
+        yield from []
 
     def _add_transaction_to_state(self, event_time: int, row):
         transaction_list = self._window_state.get(event_time)
